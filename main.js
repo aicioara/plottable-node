bd57575095eb7b5cce232237fb600ab17d970dac
Andrei Cioara <acioara@palantir.com>
2015-07-13 17:58:07 -0700

    Removed the external CSS dependency

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ function convertFile() {
 	    page.open("template.html", function (status) {
 	    	page.injectJs('script.js', function() {
 		      page.evaluate(function() {
-		      	var node = document.getElementById('chart');
+		      	var node = document.getElementById('svg');
 		      	node.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-						var nodeInformation = new XMLSerializer().serializeToString(node);
-		      	return nodeInformation;
-		      }, function (result) {
-		      	createSVGFile(result);
+						var svgData = new XMLSerializer().serializeToString(node);
+		      	return svgData;
+		      }, function (svgData) {
+		      	createSVGFile(svgData);
 		        ph.exit();
 		      });
 	    	})
@@ -23,29 +23,42 @@ function convertFile() {
 
 function createSVGFile(svgNodeData) {
 	var header1 = '<?xml version="1.0" standalone="no"?>';
-	var header2 = '<?xml-stylesheet href="plottable.css" type="text/css"?>';
-	var content = header1 + "\n" + header2 + "\n" + svgNodeData + "\n";
-	fs.writeFile('./output.svg', content, function (err, data) {
-		if (err) {
-			console.log(err);
-			return;
-		}
+	loadCSS(function(cssContent){
+		svgNodeData = svgNodeData.replace("</svg>", cssContent + "</svg>")
+		var content = header1 + svgNodeData + "\n" ;
+		fs.writeFile('./output.svg', content, function (err, data) {
+			if (err) {
+				return console.log(err);
+			}
+		})
 	})
 }
 
-function loadScript(scriptName) {
-	fs.readFile(scriptName, 'utf8', function (err,data) {
+function loadFile(scriptName, callback) {
+	fs.readFile(scriptName, 'utf8', function (err, data) {
 	  if (err) {
 	    return console.log(err);
 	  }
-	  console.log(data);
+	  callback(data);
+	});
+}
+
+function loadCSS(callback) {
+	loadFile('bower_components/plottable/plottable.css', function(stylesheet) {
+		var injection = "<defs>"
+		injection += '<style type="text/css">'
+		injection += '<![CDATA[';
+		injection += stylesheet;
+		injection += ']]>'
+		injection += '</style>';
+		injection += '</defs>';
+		callback(injection);
 	});
 }
 
 
 function start() {
 	convertFile();
-	// loadScript('script.js')
 }
 
 
