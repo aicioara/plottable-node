4cf210afb745c2a512a2401294f2c1c186d64c3e
Andrei Cioara <acioara@palantir.com>
2015-07-14 14:08:59 -0700

    Update the convert to png script

--- svgToPng.sh
+++ svgToPng.sh
@@ -1,1 +1,8 @@
-qlmanage -t -s 1000 -o . output.svg
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+	echo "Expecting exactly 1 argument, the file to be converted"
+	exit 1
+fi
+
+qlmanage -t -s 1000 -o . $1
