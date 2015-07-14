3c990eecd7946331e5a75e454cc49a8ebd926720
Andrei Cioara <acioara@palantir.com>
2015-07-14 12:06:37 -0700

    Converted indentation to spaces

--- main.js
+++ main.js
@@ -10,94 +10,94 @@ var _svgHeight = 500;
 var _svgWidth = 500;
 
 function convertFile() {
-	phantom.create(function(ph) {
-	  ph.createPage(function(page) {
-	    page.open(_templateFile, function(status) {
-	    	if (status !== "success") {
-	    		throw new Error("Could not find file " + _templateFile);
-	    	}
-	    	_prepareSVG(page);
-	    	_runPlottable(page);
-	    	_extractSVG(page);
+  phantom.create(function(ph) {
+    ph.createPage(function(page) {
+      page.open(_templateFile, function(status) {
+        if (status !== "success") {
+          throw new Error("Could not find file " + _templateFile);
+        }
+        _prepareSVG(page);
+        _runPlottable(page);
+        _extractSVG(page);
         ph.exit();
-    	});
-	  });
-	});
+      });
+    });
+  });
 }
 
 function _prepareSVG(page) {
-	page.evaluate(function(height, width) {
-		var svg =	document.createElementNS("http://www.w3.org/2000/svg", "svg");
-		svg.id = "svg";
-		svg.setAttribute("height", height);
-		svg.setAttribute("width", width);
-		document.body.appendChild(svg);
-	}, function() {
-	}, _svgHeight, _svgWidth);
+  page.evaluate(function(height, width) {
+    var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.id = "svg";
+    svg.setAttribute("height", height);
+    svg.setAttribute("width", width);
+    document.body.appendChild(svg);
+  }, function() {
+  }, _svgHeight, _svgWidth);
 }
 
 function _runPlottable(page) {
-	_dataFile && page.injectJs(_dataFile, function(success) {
-		if (!success) {
-			throw new Error("Could not find file " + data_file);
-		}
-	});
-	_testFile && page.injectJs(_testFile, function(success) {
-		if (!success) {
-			throw new Error("Could not find file " + _testFile);
-		}
-	});
+  _dataFile && page.injectJs(_dataFile, function(success) {
+    if (!success) {
+      throw new Error("Could not find file " + data_file);
+    }
+  });
+  _testFile && page.injectJs(_testFile, function(success) {
+    if (!success) {
+      throw new Error("Could not find file " + _testFile);
+    }
+  });
 }
 
 function _extractSVG(page) {
-	page.evaluate(function() {
-		var node = document.getElementById('svg');
-		node.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-		var svgData = new XMLSerializer().serializeToString(node);
-		return svgData;
-	}, function (svgData) {
-		createSVGFile(svgData);
-	});
+  page.evaluate(function() {
+    var node = document.getElementById('svg');
+    node.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    var svgData = new XMLSerializer().serializeToString(node);
+    return svgData;
+  }, function (svgData) {
+    createSVGFile(svgData);
+  });
 }
 
 function createSVGFile(svgNodeData) {
-	var header = '<?xml version="1.0" standalone="no"?>';
-	generateCSS(function(cssContent){
-		cssInjectedSvgNodeData = svgNodeData.replace("</svg>", cssContent + "</svg>")
-		var content = header + cssInjectedSvgNodeData + "\n" ;
-		fs.writeFile(_outputFile, content, function (err, data) {
-			if (err) {
-				throw new Error(err);
-			}
-		})
-	})
+  var header = '<?xml version="1.0" standalone="no"?>';
+  generateCSS(function(cssContent){
+    cssInjectedSvgNodeData = svgNodeData.replace("</svg>", cssContent + "</svg>")
+    var content = header + cssInjectedSvgNodeData + "\n" ;
+    fs.writeFile(_outputFile, content, function (err, data) {
+      if (err) {
+        throw new Error(err);
+      }
+    })
+  })
 }
 
 function _loadFile(file, callback) {
-	fs.readFile(file, 'utf8', function (err, data) {
-	  if (err) {
-	    return console.log(err);
-	  }
-	  callback(data);
-	});
+  fs.readFile(file, 'utf8', function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    callback(data);
+  });
 }
 
 function generateCSS(callback) {
-	_loadFile(_plottableCSS, function(stylesheet) {
-		var injection = "<defs>"
-		injection += '<style type="text/css">'
-		injection += '<![CDATA[';
-		injection += stylesheet;
-		injection += ']]>'
-		injection += '</style>';
-		injection += '</defs>';
-		callback(injection);
-	});
+  _loadFile(_plottableCSS, function(stylesheet) {
+    var injection = "<defs>"
+    injection += '<style type="text/css">'
+    injection += '<![CDATA[';
+    injection += stylesheet;
+    injection += ']]>'
+    injection += '</style>';
+    injection += '</defs>';
+    callback(injection);
+  });
 }
 
 
 function start() {
-	convertFile();
+  convertFile();
 }
 
 
